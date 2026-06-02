20282d661bf674c01ab4de1ae90439285765cd23
Arnau-R <arnau.rius02@estudiant.upf.edu>
2026-06-02 16:52:17 +0200

    Solucionats els errors

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          supabase: ['@supabase/supabase-js'],
+          leaflet: ['leaflet'],
+          icons: ['lucide-react'],
+        },
+      },
+    },
+  },
 })
